8905ac9c0b506b54e61cf9876d3b2a567929bdb7
Ojasv Singh <ojasv.singh@gmail.com>
2026-01-02 13:37:45 -0500

    Updated configs

--- src/main/java/com/example/ratelimiter/config/RateLimiterConfig.java
+++ src/main/java/com/example/ratelimiter/config/RateLimiterConfig.java
@@ -1,13 +1,13 @@
 package com.example.ratelimiter.config;
 
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Bucket4j;
-import io.github.bucket4j.Refill;
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.Duration;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 
 @Configuration
 public class RateLimiterConfig {
@@ -16,6 +16,6 @@ public class RateLimiterConfig {
     public Bucket createRateLimiterBucket(){
         //20tokens per minute
         Bandwidth limit = Bandwidth.classic(20,Refill.greedy(20,Duration.ofMinutes(1)));
-        return Bucket4j.builder().addLimit(limit).build();
+        return Bucket.builder().addLimit(limit).build();
     }
 }

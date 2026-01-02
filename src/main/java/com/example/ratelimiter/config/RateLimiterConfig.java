ad64e4c2b94021ca1f763aba6a88c4c6018d9042
Ojasv Singh <ojasv.singh@gmail.com>
2026-01-02 14:23:22 -0500

    create-AreaCalculationController

--- src/main/java/com/example/ratelimiter/config/RateLimiterConfig.java
+++ src/main/java/com/example/ratelimiter/config/RateLimiterConfig.java
@@ -1,21 +1,14 @@
 package com.example.ratelimiter.config;
 
-import java.time.Duration;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
 
 @Configuration
 public class RateLimiterConfig {
 
-    @Bean
-    public Bucket createRateLimiterBucket(){
-        //20tokens per minute
-        Bandwidth limit = Bandwidth.classic(20,Refill.greedy(20,Duration.ofMinutes(1)));
-        return Bucket.builder().addLimit(limit).build();
-    }
+    // @Bean
+    // public Bucket createRateLimiterBucket(){
+    //     //10tokens per minute
+    //     Bandwidth limit = Bandwidth.classic(10,Refill.greedy(10,Duration.ofMinutes(1)));
+    //     return Bucket.builder().addLimit(limit).build();
+    // }
 }
---
+++ src/main/java/com/example/ratelimiter/controller/AreaCalculationController.java
@@ -0,0 +1,40 @@
+package com.example.ratelimiter.controller;
+
+import java.time.Duration;
+
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Refill;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+
+@RestController
+public class AreaCalculationController{
+
+    private final Bucket rateLimiterBucket;
+
+    public AreaCalculationController(){
+        //10 requests per minute
+        Bandwidth limit = Bandwidth.classic(10,Refill.greedy(10,Duration.ofMinutes(1)));
+        this.rateLimiterBucket = Bucket.builder().addLimit(limit).build();
+    }
+
+
+    //endpoint path: http://localhost:8080/api/v1/area/rectangle
+
+    @PostMapping("/api/v1/area/rectangle")
+    public ResponseEntity<AreaV1> rectangle(@RequestBody RectangleDimensionsV1 dimensions){
+        //try to consume a token
+        if(rateLimiterBucket.tryConsume(1)){
+            AreaV1 result =  new AreaV1("rectangle", dimensions.getLength() * dimensions.getWidth());
+            return ResponseEntity.ok(result);
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Too many requests");
+    }
+}
